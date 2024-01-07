da7c6783134cb9ae3d33d4f2bc597de7e6c3005e
aokmen <abdullah.okmen.11@gmail.com>
2024-01-07 22:34:33 +0100

    loading's file created in Next.js project file

---
+++ next.js-03-04-Netflix_Clone/app/loading.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+//? aplikasyonun herhangi bir yerinde gecikme oluştuğunda otomatik loading.js dosyası render edilir
+const Loading = () => {
+  return (
+    <div className="text-center">
+      <div className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full text-blue-500 mt-52">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
