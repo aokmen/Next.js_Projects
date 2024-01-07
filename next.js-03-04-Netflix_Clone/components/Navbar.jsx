037f401c3decbcb35f5d8f405de419610b90b49f
aokmen <abdullah.okmen.11@gmail.com>
2024-01-07 22:23:42 +0100

    Navbar created in the movie next.js

--- next.js-03-04-Netflix_Clone/components/Navbar.jsx
+++ next.js-03-04-Netflix_Clone/components/Navbar.jsx
@@ -77,59 +77,67 @@ const Navbar = () => {
                   leaveTo="transform opacity-0 scale-95"
                 >
                   <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                    <Menu.Item>
-                      {({ active }) => (
-                        <Link
-                          href="/register"
-                          className={classNames(
-                            active ? "bg-gray-100" : "",
-                            "block px-4 py-2 text-sm text-gray-700"
+                    {!currentUser && (
+                      <>
+                        <Menu.Item>
+                          {({ active }) => (
+                            <Link
+                              href="/register"
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}
+                            >
+                              Register
+                            </Link>
                           )}
-                        >
-                          Register
-                        </Link>
-                      )}
-                    </Menu.Item>
-                    <Menu.Item>
-                      {({ active }) => (
-                        <Link
-                          href="/login"
-                          className={classNames(
-                            active ? "bg-gray-100" : "",
-                            "block px-4 py-2 text-sm text-gray-700"
+                        </Menu.Item>
+                        <Menu.Item>
+                          {({ active }) => (
+                            <Link
+                              href="/login"
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}
+                            >
+                              Login
+                            </Link>
                           )}
-                        >
-                          Login
-                        </Link>
-                      )}
-                    </Menu.Item>
-                    <Menu.Item>
-                      {({ active }) => (
-                        <Link
-                          href="/profile"
-                          className={classNames(
-                            active ? "bg-gray-100" : "",
-                            "block px-4 py-2 text-sm text-gray-700"
+                        </Menu.Item>
+                      </>
+                    )}
+                    {currentUser && (
+                      <>
+                        <Menu.Item>
+                          {({ active }) => (
+                            <Link
+                              href="/profile"
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}
+                            >
+                              Profile
+                            </Link>
                           )}
-                        >
-                          Profile
-                        </Link>
-                      )}
-                    </Menu.Item>
-                    <Menu.Item>
-                      {({ active }) => (
-                        <span
-                          className={classNames(
-                            active ? "bg-gray-100" : "",
-                            "block px-4 py-2 text-sm text-gray-700 cursor-pointer"
+                        </Menu.Item>
+                        <Menu.Item>
+                          {({ active }) => (
+                            <span
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700 cursor-pointer"
+                              )}
+                              role="button"
+                              onClick={() => logOut()}
+                            >
+                              Log out
+                            </span>
                           )}
-                          role="button"
-                          onClick={() => logOut()}
-                        >
-                          Log out
-                        </span>
-                      )}
-                    </Menu.Item>
+                        </Menu.Item>
+                      </>
+                    )}
                   </Menu.Items>
                 </Transition>
               </Menu>
